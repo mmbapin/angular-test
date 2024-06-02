ab4e37ec548e367a1e0089216bc81b325eeec411
mmbapin <bapinmm@gmail.com>
2024-06-02 16:16:10 +0600

    Save Test Case Added

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -5,6 +5,7 @@ import {
   HttpTestingController,
 } from "@angular/common/http/testing";
 import { COURSES } from "../../../../server/db-data";
+import { Course } from "../model/course";
 
 describe("CoursesService", () => {
   let coursesService: CoursesService,
@@ -43,6 +44,26 @@ describe("CoursesService", () => {
     const req = httpTestingControler.expectOne("/api/courses/12");
     expect(req.request.method).toEqual("GET");
     req.flush(COURSES[12]);
+  });
+
+  it("should save the course data", () => {
+    const changes: Partial<Course> = {
+      titles: { description: "Testing Course" },
+    };
+    coursesService.saveCourse(12, changes).subscribe((course) => {
+      expect(course.id).toBe(12);
+    });
+
+    const req = httpTestingControler.expectOne("/api/courses/12");
+    expect(req.request.method).toEqual("PUT");
+    expect(req.request.body.titles.description).toEqual(
+      changes.titles.description
+    );
+
+    req.flush({
+      ...COURSES[12],
+      ...changes,
+    });
   });
 
   afterEach(() => {
